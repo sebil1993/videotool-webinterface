836b90821c4d9dc43c15fb8cb757fa453f457301
Sebastian Lerche <s.lerche@kentix.com>
2022-09-20 17:16:28 +0200

    added font awesome

--- resources/js/Components/VideoPlayer.vue
+++ resources/js/Components/VideoPlayer.vue
@@ -1,29 +1,27 @@
 <template>
   <div @mouseenter="showControls" @mouseleave="hideControls">
     <video class="" :src="videoSrc"></video>
-    <div class="w-full h-7 flex border border-black justify-between">
+    <div class="w-full h-7 flex border border-black justify-between text-sm">
       <button
         v-if="controlsVisible == true"
         @click="playVideo"
         class="border-black border-r px-2 bg-green-200"
       >
-        ►
+        <font-awesome-icon icon="fa-solid fa-play" />
       </button>
       <button
         v-if="controlsVisible == true"
         @click="pauseVideo"
-        class="border-black border-x px-2 bg-gray-200 font-extrabold"
+        class="border-black border-x px-2 bg-gray-200"
       >
-        ||
+        <font-awesome-icon icon="fa-solid fa-pause" />
       </button>
       <button
         v-if="controlsVisible == true"
         @click="fullVideo"
-        class="border-black border-l px-2 bg-gray-200 font-extrabold"
+        class="border-black border-l px-2 bg-gray-200"
       >
-        ⤡
-
-        <label class="-ml-[19.5px]" for="">⤢</label>
+        <font-awesome-icon icon="fa-solid fa-expand" />
       </button>
     </div>
   </div>
--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,11 @@ import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faExpand, faPause, faPlay } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faExpand, faPause, faPlay)
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -14,6 +19,7 @@ createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
+            .component('font-awesome-icon', FontAwesomeIcon)
             .use(plugin)
             .use(ZiggyVue, Ziggy)
             .mount(el);
